eb809fe88a517e646f663ddafc49b9b0d67e826f
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-04-02 09:49:49 -0400

    Most functionality implemented at this point. Time for a real Alpha

---
+++ src/main/java/com/ocpsoft/forge/prettyfaces/MappingIdCompleter.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.ocpsoft.forge.prettyfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.seam.forge.project.Project;
+import org.jboss.seam.forge.shell.completer.SimpleTokenCompleter;
+
+import com.ocpsoft.pretty.faces.config.PrettyConfig;
+import com.ocpsoft.pretty.faces.config.mapping.UrlMapping;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class MappingIdCompleter extends SimpleTokenCompleter
+{
+   @Inject
+   private Project project;
+
+   @Override
+   public List<Object> getCompletionTokens()
+   {
+      List<Object> result = new ArrayList<Object>();
+      try
+      {
+         PrettyConfig config = project.getFacet(PrettyFacesFacet.class).getPrettyConfig();
+         for (UrlMapping mapping : config.getMappings())
+         {
+            if (mapping.getId() != null && !mapping.getId().isEmpty())
+               result.add(mapping.getId());
+         }
+      }
+      catch (Exception e)
+      {
+         // oh well, no completion for you
+      }
+      return result;
+   }
+
+}
--- src/main/java/com/ocpsoft/forge/prettyfaces/PrettyFacesPlugin.java
+++ src/main/java/com/ocpsoft/forge/prettyfaces/PrettyFacesPlugin.java
@@ -1,6 +1,9 @@
 package com.ocpsoft.forge.prettyfaces;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.enterprise.event.Event;
@@ -10,6 +13,7 @@ import org.jboss.seam.forge.parser.xml.XMLParser;
 import org.jboss.seam.forge.project.Project;
 import org.jboss.seam.forge.project.facets.FacetInstallationAborted;
 import org.jboss.seam.forge.resources.FileResource;
+import org.jboss.seam.forge.shell.ShellColor;
 import org.jboss.seam.forge.shell.ShellMessages;
 import org.jboss.seam.forge.shell.events.InstallFacets;
 import org.jboss.seam.forge.shell.plugins.Alias;
@@ -24,9 +28,12 @@ import org.jboss.shrinkwrap.descriptor.api.Node;
 import org.jboss.shrinkwrap.descriptor.impl.base.Strings;
 import org.xml.sax.SAXException;
 
+import com.ocpsoft.pretty.faces.annotation.URLAction.PhaseId;
 import com.ocpsoft.pretty.faces.config.PrettyConfig;
+import com.ocpsoft.pretty.faces.config.mapping.UrlAction;
 import com.ocpsoft.pretty.faces.config.mapping.UrlMapping;
 import com.ocpsoft.pretty.faces.event.MultiPageMessagesSupport;
+import com.ocpsoft.pretty.faces.url.URL;
 import com.ocpsoft.pretty.faces.url.URLPatternParser;
 
 @RequiresProject
@@ -56,81 +63,102 @@ public class PrettyFacesPlugin implements Plugin
       }
    }
 
-   // confirmed working
-   @Command("setup")
-   public void setup(PipeOut out)
-   {
-      if (!project.hasFacet(PrettyFacesFacet.class))
-      {
-         installFacets.fire(new InstallFacets(PrettyFacesFacet.class));
-      }
-      if (project.hasFacet(PrettyFacesFacet.class))
-         ShellMessages.success(out, "PrettyFaces is configured.");
-   }
-
-   @Command("map-url")
-   public void mapUrl(
+   @Command("action")
+   public void mapUrlAction(
             PipeOut out,
-            @Option(name = "pattern", description = "the URL pattern", required = true) String pattern,
-            @Option(name = "viewId", description = "the server resource to be displayed", required = true) String viewId,
-            @Option(name = "id", description = "the mapping id") String id,
-            @Option(name = "parentId", description = "parent mapping to inherit from") String parentId,
+            @Option(name = "mappingId", completer = MappingIdCompleter.class, description = "mapping to which the action will be added", required = true) String mappingId,
+            @Option(name = "methodExpression", description = "the EL action method expression (surround with quotes)", required = true) String actionExpression,
+            @Option(name = "phaseId", description = "the JSF lifecycle action phase [default: RESTORE_VIEW]") PhaseId phaseId,
+            @Option(name = "onPostback", description = "invoke action on form POST [default: true]", defaultValue = "true") boolean onPostback,
             boolean inbound) throws IOException, SAXException
    {
       assertInstalled();
+      if (!project.hasFacet(FacesFacet.class))
+      {
+         ShellMessages.info(out, "This feature requires JavaServer(tm) Faces.");
+         installFacets.fire(new InstallFacets(true, FacesFacet.class));
+      }
 
       PrettyFacesFacet pf = project.getFacet(PrettyFacesFacet.class);
       PrettyConfig prettyConfig = pf.getPrettyConfig();
-      List<UrlMapping> mappings = prettyConfig.getMappings();
 
-      // verify not already mapped
-      for (UrlMapping m : mappings)
+      UrlMapping m = prettyConfig.getMappingById(mappingId);
+      for (UrlAction a : m.getActions())
       {
-         if (m.getPattern().equals(pattern))
+         if (a.getAction().getELExpression().equals(actionExpression))
          {
-            throw new RuntimeException("Pattern is already mapped by the " + m.toString());
+            throw new RuntimeException("Action already exists [" + a + "]");
          }
       }
 
       Node config = pf.getConfig();
-      Node m = config.create("url-mapping");
-      if (id != null)
+      Node mapping = config.getSingle("url-mapping@id=" + mappingId);
+      Node action = mapping.create("action").text(actionExpression);
+      if (onPostback == false)
       {
-         m.attribute("id", id);
+         action.attribute("onPostback", onPostback);
       }
-      m.create("pattern").attribute("value", pattern);
-      m.create("view-id").attribute("value", viewId);
-
-      if (parentId != null)
+      if (phaseId != null)
       {
-         UrlMapping parent = prettyConfig.getMappingById(parentId);
-         if (parent == null)
-         {
-            throw new RuntimeException("No parent with id of [" + parentId + "]");
-         }
-         // validate the pattern
-         new URLPatternParser(parent.getPattern() + pattern);
-         m.attribute("parentId", parentId);
+         action.attribute("phaseId", phaseId);
       }
-      new URLPatternParser(pattern);
 
       pf.saveConfig(config);
+      ShellMessages.success(out, "Added new action [action" +
+               ", phaseId=\"" + (phaseId == null ? PhaseId.ANY_PHASE : phaseId) + "\"" +
+               ", onPostback=\"" + (onPostback) + "\"]" +
+               " for mapping [" + mappingId + "]");
    }
 
    @Command("list-mappings")
-   public void listMappings(PipeOut out) throws IOException, SAXException
+   public void listMappings(PipeOut out,
+            @Option(name = "sort", shortName = "s") boolean sort,
+            @Option(name = "all", shortName = "a") boolean showAll
+            ) throws IOException, SAXException
    {
       assertInstalled();
       PrettyFacesFacet pf = project.getFacet(PrettyFacesFacet.class);
       PrettyConfig config = pf.getPrettyConfig();
 
-      List<UrlMapping> mappings = config.getMappings();
+      List<UrlMapping> mappings = new ArrayList<UrlMapping>();
+      mappings.addAll(config.getMappings());
+
+      if (sort)
+      {
+         Collections.sort(mappings, new Comparator<UrlMapping>()
+         {
+            @Override
+            public int compare(UrlMapping l, UrlMapping r)
+            {
+               if (l.getPattern() == null || r.getPattern() == null)
+               {
+                  if (l.getPattern() != null)
+                     return -1;
+                  if (r.getPattern() != null)
+                     return 1;
+                  return 0;
+               }
+               return l.getPattern().compareTo(r.getPattern());
+            }
+         });
+      }
+
       for (UrlMapping m : mappings)
       {
-         out.println(m.getPattern() + " -> " + m.getViewId()
-                  + " [" + (Strings.isNullOrEmpty(m.getId()) ? "" : "id=" + m.getId() + ", ")
-                  + (Strings.isNullOrEmpty(m.getParentId()) ? "" : "id=" + m.getId() + ", ")
-                  + "outbound=" + m.isOutbound() + "]");
+         out.println(
+                  out.renderColor(ShellColor.BOLD, m.getPattern())
+                           + out.renderColor(ShellColor.ITALIC, " -> ") + m.getViewId()
+                           + " [" + (Strings.isNullOrEmpty(m.getId()) ? "" : "id=" + m.getId() + ", ")
+                           + (Strings.isNullOrEmpty(m.getParentId()) ? "" : "id=" + m.getId() + ", ")
+                           + "outbound=" + m.isOutbound() + "]");
+
+         if (showAll)
+         {
+            out.println();
+            out.println(ShellColor.BLUE, m.toString());
+            out.println();
+         }
+
       }
 
       if (mappings.isEmpty())
@@ -227,11 +255,105 @@ public class PrettyFacesPlugin implements Plugin
 
    }
 
+   @Command("add-mapping")
+   public void mapUrl(
+            PipeOut out,
+            @Option(name = "pattern", description = "the URL pattern", required = true) String pattern,
+            @Option(name = "viewId", completer = ViewIdCompleter.class, description = "the server resource to be displayed", required = true) String viewId,
+            @Option(name = "id", description = "the mapping id", required = true) String id,
+            @Option(name = "outbound", description = "rewrite outbound URLs matching this viewId", defaultValue = "true") boolean outbound,
+            @Option(name = "parentId", completer = MappingIdCompleter.class, description = "parent mapping to inherit from") String parentId,
+            boolean inbound) throws IOException, SAXException
+   {
+      assertInstalled();
+
+      PrettyFacesFacet pf = project.getFacet(PrettyFacesFacet.class);
+      PrettyConfig prettyConfig = pf.getPrettyConfig();
+      List<UrlMapping> mappings = prettyConfig.getMappings();
+
+      // verify not already mapped
+      for (UrlMapping m : mappings)
+      {
+         if (m.getPattern().equals(pattern))
+         {
+            throw new RuntimeException("Pattern is already mapped by the " + m.toString());
+         }
+      }
+
+      Node config = pf.getConfig();
+      Node m = config.create("url-mapping");
+
+      if (id != null)
+         m.attribute("id", id);
+
+      if (outbound == false)
+         m.attribute("outbound", outbound);
+
+      m.create("pattern").attribute("value", pattern);
+      m.create("view-id").attribute("value", viewId);
+
+      if (parentId != null)
+      {
+         UrlMapping parent = prettyConfig.getMappingById(parentId);
+         if (parent == null)
+         {
+            throw new RuntimeException("No parent with id of [" + parentId + "]");
+         }
+         // validate the pattern
+         new URLPatternParser(parent.getPattern() + pattern);
+         m.attribute("parentId", parentId);
+      }
+      new URLPatternParser(pattern).matches(new URL(""));
+
+      pf.saveConfig(config);
+   }
+
+   @Command("remove-mapping")
+   public void removeMapping(
+            PipeOut out,
+            @Option(name = "id",
+                     completer = MappingIdCompleter.class,
+                     description = "the mapping id",
+                     required = true) String id
+            ) throws IOException, SAXException
+   {
+      assertInstalled();
+
+      PrettyFacesFacet pf = project.getFacet(PrettyFacesFacet.class);
+      PrettyConfig prettyConfig = pf.getPrettyConfig();
+
+      UrlMapping mapping = prettyConfig.getMappingById(id);
+      if (mapping == null)
+      {
+         throw new RuntimeException("No mapping found for id [" + id
+                  + "]. To see a list of mappings, type 'prettyfaces list-mappings'");
+      }
+
+      Node config = pf.getConfig();
+      Node m = config.removeSingle("url-mapping@id=" + id);
+      if (m == null)
+      {
+         throw new RuntimeException("Could not remove " + mapping);
+      }
+
+      pf.saveConfig(config);
+      ShellMessages.success(out, "Removed " + mapping);
+   }
+
+   // confirmed working
+   @Command("setup")
+   public void setup(PipeOut out)
+   {
+      if (!project.hasFacet(PrettyFacesFacet.class))
+         installFacets.fire(new InstallFacets(PrettyFacesFacet.class));
+
+      if (project.hasFacet(PrettyFacesFacet.class))
+         ShellMessages.success(out, "PrettyFaces is configured.");
+   }
+
    private void assertInstalled()
    {
       if (!project.hasFacet(PrettyFacesFacet.class))
-      {
          throw new RuntimeException("PrettyFaces is not installed. Use 'prettyfaces setup' to get started.");
-      }
    }
 }
